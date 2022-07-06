ff743fa27d258af292580d7af568863b7c4f5271
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-07-06 23:17:35 +0100

    Added toolbar

--- app/src/main/java/com/olisemeka/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/olisemeka/whatsappclone/MainActivity.kt
@@ -2,10 +2,14 @@ package com.olisemeka.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.olisemeka.whatsappclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
