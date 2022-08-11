bb046cc496cb0db32fe1c9b74b5fe7122d6fbb01
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-08-11 16:51:09 +0100

    Created fragment_new_chat.xml

--- app/src/main/java/com/olisemeka/whatsappclone/ui/chats/ChatsFragment.kt
+++ app/src/main/java/com/olisemeka/whatsappclone/ui/chats/ChatsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import com.olisemeka.whatsappclone.R
 import com.olisemeka.whatsappclone.databinding.FragmentChatsBinding
 import com.olisemeka.whatsappclone.datasource.DataSource
 
@@ -29,6 +31,13 @@ class ChatsFragment : Fragment() {
         val chatList = DataSource.loadChats()
         val adapter = ChatListAdapter(context, chatList)
         chatsRecyclerView.adapter = adapter
+
+        binding.chatFab.setOnClickListener {
+            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navController = navHostFragment.navController
+            navController.navigate(R.id.action_viewPager_to_newChatFragment)
+
+        }
     }
 
     override fun onDestroyView() {
