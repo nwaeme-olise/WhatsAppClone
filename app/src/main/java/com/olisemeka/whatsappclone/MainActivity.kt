f161f51f0a89b3149fcf8ce977b7fce3d6a3f0a5
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-08-14 16:56:56 +0100

    Created various menu files for the different fragments

--- app/src/main/java/com/olisemeka/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/olisemeka/whatsappclone/MainActivity.kt
@@ -2,8 +2,21 @@ package com.olisemeka.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.tabs.TabLayout
 import com.olisemeka.whatsappclone.databinding.ActivityMainBinding
+import com.olisemeka.whatsappclone.ui.calls.CallsFragment
+import com.olisemeka.whatsappclone.ui.calls.NewCallFragment
+import com.olisemeka.whatsappclone.ui.chats.ChatsFragment
+import com.olisemeka.whatsappclone.ui.chats.NewChatFragment
+import com.olisemeka.whatsappclone.ui.status.StatusFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +26,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
 
 
     }
--- app/src/main/java/com/olisemeka/whatsappclone/ui/chats/ChatsFragment.kt
+++ app/src/main/java/com/olisemeka/whatsappclone/ui/chats/ChatsFragment.kt
@@ -1,10 +1,11 @@
 package com.olisemeka.whatsappclone.ui.chats
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.NavHostFragment
 import com.olisemeka.whatsappclone.R
 import com.olisemeka.whatsappclone.databinding.FragmentChatsBinding
@@ -24,7 +25,7 @@ class ChatsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
         val context = requireContext()
         val chatsRecyclerView = binding.recyclerviewChats
 
